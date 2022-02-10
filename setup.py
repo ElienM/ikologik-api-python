028f6384792268616e68c8dcd5374d9f2662f6bb
Tom Serru <tom.serru@ikologik.com>
2022-02-10 16:49:30 +0100

    Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ikologikapi',
-    version='1.1.24',
+    version='1.1.25',
     packages=find_packages(include=['ikologikapi*']),
     url='',
     license='MIT',
